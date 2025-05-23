238741fd2e5e3b0b084b19062761481f297ea916
Morlay <morlay.null@gmail.com>
2025-05-23 09:51:04 +0800

    fix: added fuzz test

--- go.mod
+++ go.mod
@@ -1,18 +1,18 @@
 module github.com/octohelm/exp
 
-go 1.24.2
+go 1.24.3
 
 tool mvdan.cc/gofumpt
 
 require github.com/octohelm/x v0.0.0-20250409031213-9c254440c2b8
 
 require (
-	github.com/go-json-experiment/json v0.0.0-20250417205406-170dfdcf87d1 // indirect
+	github.com/go-json-experiment/json v0.0.0-20250223041408-d3c622f1b874 // indirect
 	github.com/google/go-cmp v0.7.0 // indirect
 	github.com/onsi/gomega v1.37.0 // indirect
 	golang.org/x/mod v0.24.0 // indirect
 	golang.org/x/sync v0.13.0 // indirect
 	golang.org/x/text v0.24.0 // indirect
 	golang.org/x/tools v0.32.0 // indirect
-	mvdan.cc/gofumpt v0.8.0 // indirect
+	mvdan.cc/gofumpt v0.7.0 // indirect
 )
--- xiter/chunk_test.go
+++ xiter/chunk_test.go
@@ -1,6 +1,7 @@
 package xiter
 
 import (
+	"math/rand/v2"
 	"slices"
 	"testing"
 
@@ -24,4 +25,37 @@ func TestChunk(t *testing.T) {
 		{6, 7, 8},
 		{9},
 	}))
+}
+
+func FuzzChunk(f *testing.F) {
+	for range 10 {
+		f.Add(rand.IntN(50000))
+	}
+
+	f.Fuzz(func(t *testing.T, total int) {
+		src := Seq(func(yield func(int) bool) {
+			for i := range total {
+				if !yield(i) {
+					return
+				}
+			}
+		})
+
+		chunkN := 50
+
+		chunked := Chunk(src, chunkN)
+		chunks := slices.Collect(chunked)
+
+		for i, chunk := range chunks {
+			// last one
+			if i == len(chunks)-1 {
+				t.Run("last chunk", func(t *testing.T) {
+					testingx.Expect(t, len(chunk), testingx.Equal(total-chunkN*(len(chunks)-1)))
+				})
+				continue
+			}
+
+			testingx.Expect(t, len(chunk), testingx.Equal(50))
+		}
+	})
 }
